51e00b4d143b2f3b2b748ee6e8b89d3ec4931798
Junaed <junaed1995@gmail.com>
2021-11-24 03:19:27 +0600

    Initial commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kApiKey = "a4953f46fdafd22c380400a2948fca2d";
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
---
+++ scratch.dart
@@ -0,0 +1,30 @@
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String result2 = await task2();
+  task3(result2);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future task2() async {
+  String result = "";
+  Duration duration = Duration(seconds: 3);
+  await Future.delayed(duration, () {
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+
+  return result;
+}
+
+void task3(String result2) {
+  String result = 'task 3 data';
+  print('Task 3 complete with $result2');
+}
